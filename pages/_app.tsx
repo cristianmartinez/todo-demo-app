642efb53e0f51e57c1cfaea8d89f70585920928e
Cristian Martinez <dev.cmartinez@gmail.com>
2022-10-21 11:34:02 -0500

    Fix appcues import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src='https://localhost:8082/bootstrap.js' />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
